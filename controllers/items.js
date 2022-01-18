b363eb4018395e7107d01f14ea8321e277b42850
Aleksa Zdravkovic <aleksa.zdravkovic1996@gmail.com>
2022-01-18 23:48:41 +0100

    get single and delete item done

--- controllers/items.js
+++ controllers/items.js
@@ -18,4 +18,27 @@ const createItem = async (req, res) => {
   }
 };
 
-module.exports = { getAllItems, createItem };
+const getSingleItem = async (req, res) => {
+  const currentItemId = req.params.id;
+  try {
+    const item = await Item.findById({ _id: currentItemId });
+    if (!item) return res.status(404).json(`no item with id: ${currentItemId}`);
+    res.status(200).json({ item });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+const deleteItem = async (req, res) => {
+  const currentItemId = req.params.id;
+
+  try {
+    const item = await Item.findByIdAndDelete({ _id: currentItemId });
+    if (!item) return res.status(404).json(`no item with id: ${currentItemId}`);
+    res.status(200).json({ item });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+module.exports = { getAllItems, createItem, getSingleItem, deleteItem };
--- routes/items.js
+++ routes/items.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllItems, createItem } = require("../controllers/items");
+const { getAllItems, createItem, getSingleItem, deleteItem } = require("../controllers/items");
 
 const router = express.Router();
 
 router.route("/").get(getAllItems).post(createItem);
+router.route("/:id").get(getSingleItem).delete(deleteItem);
 
 module.exports = router;
